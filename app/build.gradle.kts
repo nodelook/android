a62df0eb9ba50a7b05f5dd4ee4933168507ed41f
Muhammadhussein Ammari <xmha97@gmail.com>
2026-06-20 00:02:05 +0330

    Prepare release v1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "ir.ammari.nodelook"
         minSdk = 1
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
