6ea69a194b3aa34ed7f0ea47f557c55be3130e31
Muhammadhussein Ammari <xmha97@gmail.com>
2026-07-27 20:05:47 +0330

    Update versionCode and versionName to 7 and 1.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "ir.ammari.nodelook"
         minSdk = 1
         targetSdk = 37
-        versionCode = 6
-        versionName = "1.5.0"
+        versionCode = 7
+        versionName = "1.6.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         vectorDrawables.generatedDensities()
